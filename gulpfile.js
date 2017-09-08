79f1516bfffad4a549524248776ea4d2f2cc3ead
Rae <houxiaoyu1993@126.com>
2017-09-08 16:19:31 +0800

    uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require("gulp");
+var uglify = require("gulp-uglify")
 gulp.task("test",function () {
     console.log(11);
 });
+
+
+gulp.task("script",function () {
+    gulp.src("js/jsg.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist"))
+})
